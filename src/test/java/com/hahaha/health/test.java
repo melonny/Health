8dc4002773d9af4a4d36318ef3aa9d34d2f3d00a
xuli.nofear <1017156500@qq.com>
2022-09-23 20:26:02 +0800

    new file:   src/test/java/com/hahaha/health/test.java

---
+++ src/test/java/com/hahaha/health/test.java
@@ -0,0 +1,21 @@
+package com.hahaha.health;
+
+class haha{
+    String a;
+    String b;
+    int[] p;
+}
+
+public class test{
+    haha m;
+    String c;
+
+    public test(){
+        m = new haha();
+    }
+
+
+    public static void main(String args[]){
+        test t = new test();
+    }
+}
