9b2fab898bbde55a05513c1336a0292539e7f7d4
xuli.nofear <1017156500@qq.com>
2022-10-17 19:07:08 +0800

    modified:   pom.xml 	new file:   src/main/java/com/hahaha/health/config/RedisConfig.java 	renamed:    src/main/java/com/hahaha/health/util/WebConfig.java -> src/main/java/com/hahaha/health/config/WebConfig.java 	modified:   src/main/java/com/hahaha/health/controller/UserController.java 	new file:   src/main/java/com/hahaha/health/controller/emailController.java 	new file:   src/main/java/com/hahaha/health/entity/userRegister.java 	new file:   src/main/java/com/hahaha/health/util/emailUtil.java 	new file:   src/main/java/com/hahaha/health/util/redisUtil.java 	modified:   src/main/resources/application.yml

--- src/main/java/com/hahaha/health/controller/UserController.java
+++ src/main/java/com/hahaha/health/controller/UserController.java
@@ -3,7 +3,9 @@ package com.hahaha.health.controller;
 import com.alibaba.fastjson.JSON;
 import com.hahaha.health.entity.QueryInfo;
 import com.hahaha.health.entity.User;
+import com.hahaha.health.entity.userRegister;
 import com.hahaha.health.service.userService;
+import com.hahaha.health.util.redisUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,8 @@ public class UserController {
 
     @Autowired
     userService userservice;
+    @Autowired
+    redisUtil redis;
 
     @RequestMapping("/alluser")
     public String getUserList(QueryInfo queryInfo){
@@ -36,7 +40,16 @@ public class UserController {
     }
 
     @RequestMapping("/adduser")
-    public String addUser(@RequestBody User user){
+    public String addUser(@RequestBody userRegister userregister){
+        User user = userregister.getUser();
+        if(userservice.getUserByMessage(user.getUsername(), user.getPassword())!=null){
+            return "exit";
+        }
+        String validCode = userregister.getCode();
+        String redisCode = redis.get(user.getEmail());
+        if(!redisCode.equals(validCode)){
+            return "codeError";
+        }
         user.setRole("普通用户");
         user.setState(false);
         int i = userservice.addUser(user);
---
+++ src/main/java/com/hahaha/health/controller/emailController.java
@@ -0,0 +1,28 @@
+package com.hahaha.health.controller;
+
+import com.hahaha.health.util.emailUtil;
+import com.hahaha.health.util.redisUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class emailController {
+    @Autowired
+    redisUtil redis;
+    @Value("${spring.mail.code.time}")
+    private int validTime;
+    @Autowired
+    private emailUtil se;
+    @RequestMapping("/code")
+    public String sendCode(@RequestParam("email")String email){
+        System.out.println(email);
+        String code = se.sendEmail(email);
+        redis.set(email,code);
+        System.out.println(redis.get(email));
+        System.out.println(email+":"+code);
+        return "Success";
+    }
+}
